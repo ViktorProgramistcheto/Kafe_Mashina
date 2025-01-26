59e5f1c7560b9676f0dfc8438898b057273c96b9
Vikdev <137683725+ViktorProgramistcheto@users.noreply.github.com>
2025-01-26 21:22:02 +0200

    Add files via upload

---
+++ ConsoleApp1/KafeMashina.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class KafeMashina
+    {
+        private int mliturVoda; // в милилитри
+        private int gramaKafe; // в грама
+        private bool vkljucheno;
+
+   
+        public int MliturVoda
+        {
+            get { return mliturVoda; }
+            set { mliturVoda = value; }
+        }
+
+        public int GramaKafe
+        {
+            get { return gramaKafe; }
+            set { gramaKafe = value; } 
+        }
+
+        public bool Vkljucheno
+        {
+            get { return vkljucheno; }
+            set { vkljucheno = value; }
+        }
+
+       
+
+
+
+        public KafeMashina()
+        {
+            mliturVoda = 1000; 
+            gramaKafe = 200; 
+            vkljucheno = false; 
+        }
+
+
+
+
+
+
+
+        public void TurnOn()
+        {
+            if (vkljucheno)
+            {
+                Console.WriteLine("Кафе машината вече е включена.");
+            }
+            else
+            {
+                vkljucheno = true;
+                Console.WriteLine("Кафе машината е включена!");
+            }
+        }
+
+        public void MakeCoffee(string type)
+        {
+            if (!vkljucheno)
+            {
+                Console.WriteLine("Машината е изключена!");
+                return;
+            }
+
+            int waterNeeded = 0;
+            int coffeeNeeded = 0;
+
+         
+                Console.WriteLine("Изчакайте!");
+
+                waterNeeded = 200;
+                coffeeNeeded = 10;
+
+
+            if (mliturVoda >= waterNeeded && gramaKafe >= coffeeNeeded)
+            {
+                mliturVoda -= waterNeeded;
+                gramaKafe -= coffeeNeeded;
+
+                Console.WriteLine($"{type} е готово! Насладете се!");
+            }
+            else
+            {
+                Console.WriteLine("Недостатъчно вода или кафе. Заредете машината.");
+            }
+        }
+
+        public void Zaredi()
+        {
+            mliturVoda = 1000;
+            gramaKafe = 200;
+            Console.WriteLine("Машината е заредена!");
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Ниво на водата: {mliturVoda} мл");
+            Console.WriteLine($"Ниво на кафето: {gramaKafe} г");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            KafeMashina myMachine = new KafeMashina();
+
+            Console.WriteLine("Добре дошли!");
+
+            myMachine.DisplayStatus();
+
+            while (true)
+            {
+                Console.WriteLine("Изберете действие:");
+                Console.WriteLine("1. Включете машината");
+                Console.WriteLine("2. Направете кафе");
+                Console.WriteLine("3. Допълнете ресурси");
+                Console.WriteLine("4. Проверете състоянието");
+                Console.WriteLine("5. Изключи");
+
+                string izbor = Console.ReadLine()?.Trim();
+
+                if (izbor == "1")
+                {
+                    myMachine.TurnOn();
+                }
+                else if (izbor == "2")
+                {
+                    Console.WriteLine("Въведете вид кафе");
+                    string coffeeType = Console.ReadLine();
+
+                    myMachine.MakeCoffee(coffeeType);
+                }
+                else if (izbor == "3")
+                {
+                    myMachine.Zaredi();
+                }
+                else if (izbor == "4")
+                {
+                    myMachine.DisplayStatus();
+                }
+                else if (izbor == "5")
+                {
+                    Console.WriteLine("Довиждане!");
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("Невалиден избор. Опитайте отново.");
+                }
+
+
+
+            }
+        }
+    }
+}
